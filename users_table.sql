6ad8d2319118dd5e61f8a41ce7d2779008a68032
Louis DeLosSantos <ldelossa.ld@gmail.com>
2018-01-06 17:34:04 -0500

    Add constraints for email and password. Cleanup sql file

--- users_table.sql
+++ users_table.sql
@@ -6,15 +6,27 @@ CREATE INDEX idxginusers ON users USING GIN (u);
 
 -- Force username to be unique
 CREATE UNIQUE INDEX users_username ON users( (u->>'username') );
+-- Force id to be unique
+CREATE UNIQUE INDEX users_username ON users( (u->>'id') );
+-- Force email to be unique
+CREATE UNIQUE INDEX users_username ON users( (u->>'email') );
 
 -- Force username to be non-null
 ALTER TABLE users ADD CONSTRAINT username_must_exist CHECK (u ? 'username');
+-- Force id to be non-null
+ALTER TABLE users ADD CONSTRAINT id_must_exist CHECK (u ? 'id');
+-- Force email to be non-null
+ALTER TABLE users ADD CONSTRAINT email_must_exist CHECK (u ? 'email');
+--Force password to be non-null
+ALTER TABLE users ADD CONSTRAINT password_must_exist CHECK (u ? 'password');
 
 -- Force username not to be empty string
 ALTER TABLE users ADD CONSTRAINT username_must_not_be_empty_string CHECK ((u->>'username') != '');
-
---Force password to be non-null
-ALTER TABLE users ADD CONSTRAINT username_must_exist CHECK (u ? 'password');
-
+-- Force id not to be empty string
+ALTER TABLE users ADD CONSTRAINT id_must_not_be_empty_string CHECK ((u->>'id') != '');
+-- Force email not to be empty string
+ALTER TABLE users ADD CONSTRAINT email_must_not_be_empty_string CHECK ((u->>'email') != '');
 -- Force password not to be empty string
 ALTER TABLE users ADD CONSTRAINT password_must_not_be_empty_string CHECK ((u->>'password') != '');
+
+
